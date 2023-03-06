ad8f417e1ecfc9407db67749bee17547e417932a
Oriol <oriolmartinez314@gmail.com>
2023-03-06 11:30:29 +0100

    Update attention module

--- models/model_v1.py
+++ models/model_v1.py
@@ -22,23 +22,24 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, dim, num_heads=8, proj_drop=0., attn_drop=0.):
         super().__init__()
         self.num_heads = num_heads
-        # head_dim = dim // num_heads
-        self.qkv = nn.Linear(dim, dim * 3)  # (W, H, C) -> (W, H, C * 3)
+        # self.head_dim = dim // num_heads
+        self.qkv = nn.Linear(dim, dim * 3)  # (B, N, C) -> (B, N, C * 3)
         self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop)
         self.attn_drop = nn.Dropout(attn_drop)
 
     def forward(self, x):
-        W, H, C = x.shape
-        qkv = self.qkv(x).reshape(W, H, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)  # (3, head, C/head, W, H)
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)  # (3, head, C/head, W, H)
         q, k, v = qkv[0], qkv[1], qkv[2]
 
-        attn = torch.mul(q, k.transpose(-2, -1))  # sometimes they use @ operator? 
-        # attn = attn * (head_dim ** -0.5)
+        attn = q @ k.transpose(-2, -1)  # @ operator? 
+        # attn = attn * (self.head_dim ** -0.5)
         attn = attn.softmax(dim=-1)
         attn = self.attn_drop(attn)
 
-        x = torch.mul(attn, v).reshape(W, H, C)
+        x = (attn @ v).transpose(1, 2)
+        x = x.reshape(B, N, C)
         x = self.proj(x)
         x = self.proj_drop(x)
 
