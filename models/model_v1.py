45cb6afba7ed70a676f8a77bfce8ac6b161dc924
David Pujol <david.pujolperich@gmail.com>
2023-03-28 12:17:58 +0200

    Wrote some basic comments of the provided code, as well as a super basic debugging script

--- models/model_v1.py
+++ models/model_v1.py
@@ -12,10 +12,11 @@ import hydra
 #   - N x encoder block (multihead attention + LN + MLP)
 #   - MLP
 
+
 class PatchTokenization(nn.Module):
-    """ 
+    """
         Video to Patch Embedding.
-        Applies Conv2d to transform the input video 
+        Applies Conv2d to transform the input video
         (batch, chanels, frames, width, height) -> (frames, patches, embed_dim)
     """
     def __init__(self, img_size: int, patch_size: int, in_chans: int, embed_dim: int):
@@ -32,9 +33,9 @@ class PatchTokenization(nn.Module):
     def forward(self, x):
         _, _, T, _, W = x.shape
         x = rearrange(x, 'b c t w h -> (b t) c w h')
-        x = self.proj(x)
+        x = self.proj(x)    # shape: BT x F x patches_w x patches_h
         W = x.size(-1)
-        x = rearrange(x, 'b c w h -> b (w h) c')
+        x = rearrange(x, 'b c w h -> b (w h) c')     # TODO: Check because this has shape (BT, total_num_patches, F)
         return x, T, W
 
 
@@ -62,16 +63,17 @@ class MultiHeadAttention(nn.Module):
         q, k, v = qkv[0], qkv[1], qkv[2]
 
         output = self.attention.apply_attention(Q=q, K=k, V=v)
-        
+
         return output
+
 
 
 class MLP(nn.Module):
     """
         Applies 2 linear layers after attention is computed.
-        Should have as output a tensor of the same size as the patch tokens. 
+        Should have as output a tensor of the same size as the patch tokens.
     """
-    def __init__(self, in_features: int, hidden_features: int = None, out_features: int = None, 
+    def __init__(self, in_features: int, hidden_features: int = None, out_features: int = None,
                  act_layer: nn.Module = nn.GELU, drop: float = 0.):
         super().__init__()
         out_features = out_features or in_features
@@ -94,9 +96,9 @@ class MLP(nn.Module):
 
 class Block(nn.Module):
     """
-        Implements the NL + MultiheadAttention + NL + MLP block 
+        Implements the NL + MultiheadAttention + NL + MLP block
     """
-    def __init__(self, cfg: OmegaConf, dim: int, num_heads: int, mlp_ratio: float, proj_drop: float, 
+    def __init__(self, cfg: OmegaConf, dim: int, num_heads: int, mlp_ratio: float, proj_drop: float,
                  attn_drop:float, act_layer: nn.Module = nn.GELU, norm_layer: nn.Module = nn.LayerNorm):
         super().__init__()
         self.norm1 = norm_layer(dim)
@@ -115,10 +117,10 @@ class Model(nn.Module):
     """
     Model class with PatchTokenization + (MuliHeadAttention + MLP) x L + classification head
     """
-    def __init__(self, cfg: OmegaConf, mlp_ratio: float = 4., proj_drop: float = 0., attn_drop: float = 0., 
+    def __init__(self, cfg: OmegaConf, mlp_ratio: float = 4., proj_drop: float = 0., attn_drop: float = 0.,
                  norm_layer: nn.Module = nn.LayerNorm, num_frames: int = 30, dropout: float = 0.):
         super().__init__()
-        
+
         self.num_classes = cfg.NUM_CLASSES
         self.img_size = cfg.FRAME_SIZE
         self.patch_size = cfg.PATCH_SIZE
@@ -128,46 +130,60 @@ class Model(nn.Module):
 
         self.num_features = self.embed_dim = (self.patch_size * self.patch_size) * self.in_chans
         self.dropout = nn.Dropout(dropout)
-        
+
         self.num_frames = num_frames # TODO: How do we handle variable number of frames
 
         self.patch_embed= PatchTokenization(
-            img_size=self.img_size, 
-            patch_size=self.patch_size, 
-            in_chans=self.in_chans, 
+            img_size=self.img_size,
+            patch_size=self.patch_size,
+            in_chans=self.in_chans,
             embed_dim=self.embed_dim)
-        
+
         num_patches = self.patch_embed.num_patches
-    
+
         # Positional Embeddings
         self.cls_token = nn.Parameter(torch.zeros(1, 1, self.embed_dim))
         self.pos_embed = nn.Parameter(torch.zeros(num_frames, num_patches+1, self.embed_dim))
         # self.time_embed = nn.Parameter(torch.zeros(1, num_frames, embed_dim))
-                                       
+
         # Attention Blocks
         self.blocks = nn.ModuleList([
             Block(cfg, self.embed_dim, self.num_heads, mlp_ratio, proj_drop, attn_drop, act_layer=nn.GELU, norm_layer=norm_layer)
-            for i in range(self.depth)])                            
+            for i in range(self.depth)])
         self.norm = norm_layer(self.embed_dim)
-        
+
         # Classifier head
         self.head = nn.Linear(self.embed_dim, self.num_classes)
-        
+
     def forward(self, x):
         x, _, _ = self.patch_embed(x)
         # add class token
         cls_tokens = self.cls_token.expand(x.size(0), -1, -1) # (1, 1, embed) -> (30, 1, embed)
-        x = torch.cat((cls_tokens, x), dim=1) # (batch x frames, patches, embed) -> (batch x frames, patches + 1, embed)
+        x = torch.cat((cls_tokens, x), dim=1) # (batch x frames, patches, embed) -> (batch x frames, patches + 1, embed)    # TODO: We want video-level sequences, so shape B x TP+1 x F
         # add positional/temporal embedding
         x = x + self.pos_embed
         for block in self.blocks:
             x = block.forward(x)
         x = rearrange(x, '(b f) p e -> b f p e', f=self.num_frames) # (batch x frames, patches, embed) -> (batch, frames, patch, embed)
+
+        # TODO: You are averaging all the patches of each of the videos.
+        # TODO: Problem: You applied attention frame-wise. Also, this is what we use the CLS token for!
         x = torch.mean(x, [1,2])
         x = self.head(x)
-        return x   
+        return x
 
 
 
 
 
+
+# Debugging code
+'''
+if __name__ == '__main__':
+    # Define an input of shape 2x3x10x32x32 (BxCxTxHxW
+    x = torch.randn(2, 3, 10, 32, 32)
+
+    # Check the patchification
+    patch_embed = PatchTokenization(img_size=32, patch_size=4, in_chans=3, embed_dim=48)
+    print(patch_embed(x)[0].shape)
+'''
--- tasks/EpicKitchens/data_downloading/untar.py
+++ tasks/EpicKitchens/data_downloading/untar.py
@@ -13,24 +13,30 @@ def untar_dirs(path):
             tar_files = glob.glob(kitchen_path + '/*.tar')
 
             for file in tar_files:
-                print("Untaring file...", file)
-                # Extract the kitchen name
-                name = file.split('/')[-1]
-                name = name.split('.')[0]
+                try:
+                    print("Untaring file...", file)
+                    # Extract the kitchen name
+                    name = file.split('/')[-1]
+                    name = name.split('.')[0]
 
-                # Create a directory
-                dir_path = '/'.join(file.split('/')[:-1])
-                final_dir_path = os.path.join(dir_path, name)
-                if not os.path.exists(final_dir_path):
-                    os.makedirs(final_dir_path)
+                    # Create a directory
+                    dir_path = '/'.join(file.split('/')[:-1])
+                    final_dir_path = os.path.join(dir_path, name)
+                    if not os.path.exists(final_dir_path):
+                        os.makedirs(final_dir_path)
 
-                # Untar the file in that directory
-                tar = tarfile.open(file)
-                tar.extractall(path=final_dir_path)  # untar file into same directory
-                tar.close()
+                    # Untar the file in that directory
+                    tar = tarfile.open(file)
+                    tar.extractall(path=final_dir_path)  # untar file into same directory
+                    tar.close()
 
-                # Remove the tar file
-                os.remove(file)
+                    # Remove the tar file
+                    os.remove(file)
+
+                except Exception as e:
+                    print(f"Error occurred while processing {file}: {str(e)}")
+                    continue
+
 
 if __name__ == '__main__':
-    untar_dirs(path='/data-slow/datasets/EpicKitchens/FULL_EPIC_KITCHENS')
+    untar_dirs(path='/data-slow/datasets/EpicKitchens/FULL_EPIC_KITCHENS/')
