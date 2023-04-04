af982ba75f1e8a46a1d5b61bbcaf9b4dabbf261c
Oriol <oriolmartinez314@gmail.com>
2023-04-04 13:37:55 +0200

    Open images with PIL and do the necessary preprocess

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -7,6 +7,7 @@ import torchvision.transforms as T
 import glob
 from einops import rearrange
 import pandas as pd
+from PIL import Image
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, data_dirs):
@@ -67,11 +68,15 @@ class Dataset(torch.utils.data.Dataset):
         frame_paths = [clip_dir + 'frame_' + str(x).rjust(10, '0') + '.jpg' for x in range(clip_info['start_frame'] , clip_info['stop_frame'])]
         total_frames = len(frame_paths)   
 
-        # Load rgb image with shape (3, 1920, 1080). 
-        # Resize it to FRAME_SIZE: (3, 224, 224)
-        # Unsqueeze it to add temporal dimension. Shape: (3, 224, 224, 1)
-        # Concat all frames. Shape: (3, 1920, 1080, frames)
-        frames = [resize(torch.load(x, map_location=torch.device('cpu'))).unsqueeze(-1) for x in frame_paths]
+        #  frames = [resize(torch.tensor(np.asarray(Image.open(x))).to('cpu')).unsqueeze(-1) for x in frame_paths]
+        
+        # Load rgb frames with shape (1920, 1080, 3). 
+        frames = [torch.tensor(np.asarray(Image.open(x))).to('cpu') for x in frame_paths]
+        # rearrange to allow resize
+        frames = [rearrange(x, 'h w c -> c h w') for x in frames]
+        # Resize it to FRAME_SIZE and add temporal dimension: (3, 224, 224, 1)
+        frames = [resize(x).unsqueeze(-1) for x in frames]
+         # Concat all frames. Shape: (3, 1920, 1080, frames)
         clip = torch.cat(frames, dim=-1)
 
         # apply padding if total frames aren't enough
