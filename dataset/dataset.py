1c39030b9221ad7d6d7d0fba34505090b28a9cb1
Oriol <oriolmartinez314@gmail.com>
2023-04-04 12:48:37 +0200

    Get all frame paths, preprocess frame and create video tensor

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,7 +1,9 @@
 import numpy as np
 import torch
 import torch.utils.data
+import torch.nn.functional as F
 import torchvision
+import torchvision.transforms as T
 import glob
 from einops import rearrange
 import pandas as pd
@@ -55,12 +57,36 @@ class Dataset(torch.utils.data.Dataset):
         clip_info = self.clip_df.iloc[index]
         clip_dir = clip_info['participant_id'] + '/rgb_frames/' + clip_info['video_id']
 
-        middel_frame = clip_info['start_frame'] + (clip_info['stop_frame'] - clip_info['start_frame']) // 2
-        clip_frames = (middel_frame - self.num_frames // 2, middel_frame + self.num_frames // 2)
-        clip_paths = [clip_dir + 'frame_' + str(x).rjust(10, '0') + '.jpg' for x in range(clip_frames[0], clip_frames[1])]
+        resize = T.Resize(size=(self.frame_size, self.frame_size))
 
-        data = torch.load(file_path,map_location=torch.device('cpu'))
-        # data = torch.load(self.data_dirs[index],map_location=torch.device('cpu'))
+        # First idea: only load some frames to lighten computation
+        # middel_frame = clip_info['start_frame'] + (clip_info['stop_frame'] - clip_info['start_frame']) // 2
+        # clip_frames = (middel_frame - self.num_frames // 2, middel_frame + self.num_frames // 2)
+        
+        # Second idea: load all frames and do preprocessing
+        frame_paths = [clip_dir + 'frame_' + str(x).rjust(10, '0') + '.jpg' for x in range(clip_info['start_frame'] , clip_info['stop_frame'])]
+        total_frames = len(frame_paths)   
+
+        # Load rgb image with shape (3, 1920, 1080). 
+        # Resize it to FRAME_SIZE: (3, 224, 224)
+        # Unsqueeze it to add temporal dimension. Shape: (3, 1, 224, 224)
+        # Concat all frames. Shape: (3, frames, 1920, 1080)
+        frames = [resize(torch.load(x, map_location=torch.device('cpu'))).unsqueeze(1) for x in frame_paths]
+        video = torch.cat(frames)
+
+        if total_frames < self.num_frames:
+            missing_frames = self.num_frames - total_frames
+
+            # check if missing frames is odd in order to ensure that after padding, 
+            # num_frames equals NUM_FRAMES
+            if missing_frames % 2 == 0:
+                pad = (0, 0, 0, 0, missing_frames // 2, missing_frames // 2)
+            else:
+                pad = (0, 0, 0, 0, missing_frames // 2 + 1, missing_frames // 2)
+            
+            video = F.pad(video, pad, 'constant', 0)
+        elif total_frames > self.num_frames:
+            
     
         # Process the data if necessary
         video_data = data['data'].squeeze(0)    # shape: T x C
