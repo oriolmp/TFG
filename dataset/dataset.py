245f8961c08f3a5da518faeaff483edfae31f39a
Oriol <oriolmartinez314@gmail.com>
2023-04-05 08:52:06 +0200

    fix typos and add last rearrange

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -49,13 +49,13 @@ class Dataset(torch.utils.data.Dataset):
         self.frame_size = cfg.dataset.FRAME_SIZE
         self.frames_dir = frames_dir
         info_df = pd.read_csv(annotations_file)
-        self.clips_df = info_df[['partcipant_id', 'video_id', 'start_frame', 'stop_frame', 'verb_class']]
+        self.clips_df = info_df[['participant_id', 'video_id', 'start_frame', 'stop_frame', 'verb_class']]
 
     def __len__(self):
-        return len(self.clip_df.shape[0])
+        return len(self.clips_df.shape[0])
 
     def __getitem__(self, index):
-        clip_info = self.clip_df.iloc[index]
+        clip_info = self.clips_df.iloc[index]
         clip_dir = clip_info['participant_id'] + '/rgb_frames/' + clip_info['video_id']
 
         resize = T.Resize(size=(self.frame_size, self.frame_size))
@@ -65,7 +65,7 @@ class Dataset(torch.utils.data.Dataset):
         # clip_frames = (middel_frame - self.num_frames // 2, middel_frame + self.num_frames // 2)
         
         # Second idea: load all frames and do preprocessing
-        frame_paths = [clip_dir + 'frame_' + str(x).rjust(10, '0') + '.jpg' for x in range(clip_info['start_frame'] , clip_info['stop_frame'])]
+        frame_paths = [clip_dir + 'frame_' + str(x).rjust(10, '0') + '.jpg' for x in range(clip_info['start_frame'], clip_info['stop_frame'])]
         total_frames = len(frame_paths)   
 
         #  frames = [resize(torch.tensor(np.asarray(Image.open(x))).to('cpu')).unsqueeze(-1) for x in frame_paths]
@@ -103,6 +103,9 @@ class Dataset(torch.utils.data.Dataset):
             clip = rearrange(clip, 'c w h t1 -> c (w h) t1')
             clip = pool(clip)
             clip = rearrange(clip, 'c (w h) t2 -> c w h t2', h=self.frame_size)   
+
+        # rearrange to fit model
+        clip = rearrange(clip, 'c w h t -> c t w h')
 
         labels = clip_info['verb_class']
         
