220f2c7954b51aeef927eac6bf2c960a900767fc
Oriol <oriolmartinez314@gmail.com>
2023-04-15 11:46:35 +0200

    Switch to local mode

--- attention_zoo/base_attention.py
+++ attention_zoo/base_attention.py
@@ -6,12 +6,12 @@ class BaseAttention(nn.Module):
     @staticmethod
     def init_att_module(att_config: OmegaConf, n:int, h:int, in_feat:int, out_feat:int, debug: bool = False, **kwargs):
         # Load the configuration passed as parameter
-        model_config = att_config
-        att_name = model_config.model.ATTENTION
+        model_config = att_config.model
+        att_name = model_config.ATTENTION
 
         # Switch between paths in case you want to run it at the server/local
-        # sys.path.append(r'C:\Users\34609\VisualStudio\TFG\attention_zoo')
-        sys.path.append('/home-net/omartinez/TFG/attention_zoo')
+        sys.path.append(r'C:\Users\34609\VisualStudio\TFG\attention_zoo')
+        # sys.path.append('/home-net/omartinez/TFG/attention_zoo')
 
         # This method takes as input a name of an attention mechanism, and if implemented,
         # returns an instance of the corresponding object.
