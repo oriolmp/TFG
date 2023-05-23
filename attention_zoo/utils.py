20e39558f4f8e88bc06f9a93c65d5b6f057dab7c
Oriol <oriolmartinez314@gmail.com>
2023-05-23 13:07:13 +0200

    Ensure we take non Nan values

--- attention_zoo/utils.py
+++ attention_zoo/utils.py
@@ -11,15 +11,27 @@ def iterative_inv(X: Tensor, iters: int = 6) -> Tensor:
     abs_x: Tensor = torch.abs(X)
     col: Tensor = abs_x.sum(dim = -1)
     row: Tensor = abs_x.sum(dim = -2)
-    z: Tensor = rearrange(X, '... i j -> ... j i') / (torch.max(col) * torch.max(row))
+    print(f'row: {torch.max(col[~torch.isnan(col)])}') 
+    print(f'col: {torch.max(row[~torch.isnan(row)])}')
+    z: Tensor = rearrange(X, '... i j -> ... j i') / (torch.max(col[~torch.isnan(col)]) * torch.max(row[~torch.isnan(row)])) # ensure non nan values are used
+    print(f'z: {torch.isnan(z).any()}')
 
     I: Tensor = torch.eye(X.shape[-1], device = device)
     I: Tensor = rearrange(I, 'i j -> () i j')
+    print(f'I: {torch.isnan(I).any()}')
 
-    for _ in range(iters):
+    out = z
+    for i in range(iters):
         xz: Tensor = X @ z
+        # print(f'xz {i}: {torch.isnan(xz).any()}')
         z: Tensor = 0.25 * z @ (13 * I - (xz @ (15 * I - (xz @ (7 * I - xz)))))
-    return z
+        # print(f'z {i}: {torch.isnan(z).any()}')
+        if not torch.isnan(z).any():
+            out = z
+        else:
+            break     
+
+    return out
 
 def init_(tensor:Tensor):
     dim = tensor.shape[-1]
