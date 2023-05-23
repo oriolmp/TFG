5bae722bd424e06cd4e93341fd4c63343b02aa79
Oriol <oriolmartinez314@gmail.com>
2023-05-23 14:57:57 +0200

    Remove control print

--- attention_zoo/utils.py
+++ attention_zoo/utils.py
@@ -11,14 +11,10 @@ def iterative_inv(X: Tensor, iters: int = 6) -> Tensor:
     abs_x: Tensor = torch.abs(X)
     col: Tensor = abs_x.sum(dim = -1)
     row: Tensor = abs_x.sum(dim = -2)
-    print(f'row: {torch.max(col[~torch.isnan(col)])}') 
-    print(f'col: {torch.max(row[~torch.isnan(row)])}')
     z: Tensor = rearrange(X, '... i j -> ... j i') / (torch.max(col[~torch.isnan(col)]) * torch.max(row[~torch.isnan(row)])) # ensure non nan values are used
-    print(f'z: {torch.isnan(z).any()}')
 
     I: Tensor = torch.eye(X.shape[-1], device = device)
     I: Tensor = rearrange(I, 'i j -> () i j')
-    print(f'I: {torch.isnan(I).any()}')
 
     out = z
     for i in range(iters):
