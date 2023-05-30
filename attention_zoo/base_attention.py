6060582e4d3fe8a6959a828fe2c83a495985ce00
Oriol <oriolmartinez314@gmail.com>
2023-05-30 17:12:29 +0200

    Delete attention options not used

--- attention_zoo/base_attention.py
+++ attention_zoo/base_attention.py
@@ -18,42 +18,17 @@ class BaseAttention(nn.Module):
         if att_name == 'vanilla_attention':
             from attentions.vanilla_attention.vanilla_attention import VanillaAttention
             att_mech = VanillaAttention(model_config, n, h, in_feat, out_feat)
-        elif att_name == 'rela_attention':
-            from attentions.rela_attention.rela_attention import RelaAttention
-            att_mech = RelaAttention(model_config, n, h, in_feat, out_feat)
-        elif att_name == 'skyformer':
-            from attentions.skyformer.skyformer import SkyformerAttention
-            att_mech = SkyformerAttention(model_config, n, h, in_feat, out_feat)
         elif att_name == 'cosformer':
             from attentions.cosformer.cosformer import CosformerAttention
             att_mech = CosformerAttention(model_config, n, h, in_feat, out_feat)
-        elif att_name == 'galerkin':
-            from attentions.galerkin_transformer.galerkin_transformer import GalerkinAttention
-            att_mech = GalerkinAttention(model_config, n, h, in_feat, out_feat)
-        elif att_name == 'fastformer':
-            from attentions.fastformer.fastformer import FastFormerAttention
-            att_mech = FastFormerAttention(model_config, n, h, in_feat, out_feat)
-        elif att_name == 'linear_attention':
-            from attentions.linear_attention.linear_attention import LinearAttention
-            att_mech = LinearAttention(model_config, n, h, in_feat, out_feat)
         elif att_name == 'linformer':
             from attentions.linformer.linformer import LinformerAttention
             att_mech = LinformerAttention(model_config, n, h, in_feat, out_feat)
-        elif att_name == 'performer':
-            from attentions.performer.performer import PerformerAttention
-            att_mech = PerformerAttention(model_config, n, h, in_feat, out_feat)
         elif att_name == 'nystromformer':
             from attentions.nystromformer.nystrom_attention import NystromformerAttention
             att_mech = NystromformerAttention(model_config, n, h, in_feat, out_feat)
-        elif att_name == 'custom_full_attention':
-            from attentions.custom_full_attention.custom_full_attention import CustomFullAttention
-            att_mech = CustomFullAttention(model_config, n, h, in_feat, out_feat)
-        elif att_name == 'custom_nystrom_attention':
-            from attentions.custom_nystrom_attention.custom_nystrom_attention import \
-                CustomNystromAttention
-            att_mech = CustomNystromAttention(model_config, n, h, in_feat, out_feat)
         else:
             raise (
-                'The name of the selected attention mechanism is not implemented. \n We support self-attention, rela_attention, skyformer, scatterbrain, cosformer, galerkin, fastformer, linear_attention, linformer, performer, nyströmformer, RNNS, custom_full_attention and custom_nystrom_attention')
+                'The name of the selected attention mechanism is not implemented. \n We support self-attention, cosformer, linformer,  nyströmformer')
 
         return att_mech
