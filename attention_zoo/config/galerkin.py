bf5fb9e989c6fe67f225fb7ee05ce6b23bdbe270
Oriol <oriolmartinez314@gmail.com>
2023-06-12 12:59:22 +0200

    Delete cfg files for unused attention mechanisms

--- attention_zoo/config/galerkin.py
+++
@@ -1,7 +0,0 @@
-from attrs import define, field
-from attention_zoo.config.abstract_attention_config import AbstractAttentionConfig
-
-
-@define
-class AttentionConfig(AbstractAttentionConfig):
-    name:str = field(default='galerkin')
--- attention_zoo/config/performer.py
+++
@@ -1,9 +0,0 @@
-from attrs import define, field
-from attention_zoo.config.abstract_attention_config import AbstractAttentionConfig
-
-
-@define
-class AttentionConfig(AbstractAttentionConfig):
-    name:str = field(default='performer')
-    kernel_type:str = field(default='relu')
-    rp_dim:int = field(default=256)
